626fb8b2f923a66fcc1acbe397ed4dfb4ae0a56c
Abhishek Anand <aanandmailbox@gmail.com>
2013-08-27 19:21:25 -0400

    Added Function help

--- LTP-Python/triangle.py
+++ LTP-Python/triangle.py
@@ -1,8 +1,33 @@
+
 #Week 1 : Lecture Video Example
 
 def area(base, height):
     return base * height / 2
 
-
 def perimeter(side1, side2, side3):
-    rerurn side1 +side2 +side3
+    '''(number, number, number) -> number
+
+    Return the perimeter of a triangle with sides of length
+    side1, side2 and side3.
+
+    >>> perimeter(3, 4, 5)
+    12
+    >>> perimeter(10.5, 6, 9.3)
+    25.8
+    '''
+    return side1 + side2 + side3
+
+
+def semiperimeter(side1, side2, side3):
+    '''(number, number, number) -> float
+
+    Return the perimeter of a triangle with sides of
+    length side1, side2 and side3.
+
+    >>> semiperimeter(3, 4, 5)
+    6.0
+    >>> semiperimeter(10.5, 6, 9.3)
+    12.9
+    '''    
+    return perimeter(side1, side2, side3) / 2
+    
